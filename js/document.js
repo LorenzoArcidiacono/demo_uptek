f3a04456be4063ae8a0b9f09cfeb0ae8c54e40b4
Arci IoT <arci0066@gmail.com>
2023-02-01 15:54:58 +0100

    create js file for each page

---
+++ js/document.js
@@ -0,0 +1,97 @@
+let type = ''
+
+// SHOW FIRST MODAL
+$('.flex-container button').click(() => {
+    $('#position-modal').removeClass('hidden');
+})
+
+$('#scan-card').click(() => type = 'card')
+$('#scan-passport').click(() => type = 'passport')
+
+// SCAN
+$('#position-modal button').click(async () => {
+
+    var code = '';
+    var result;
+
+    //if id-card read barcode
+    if (type == 'card') {
+        console.log(`scan card`);
+
+        await $.get("../core/operation.php", {
+            op: "barcode"
+        }, ).done((message) => {
+
+            message = JSON.parse(message);
+            //error while reading
+            if (message['result'] == false) {
+                console.log(message['data']);
+            } else {
+                code = message['data']
+                console.log(`code:${code}`);
+            }
+        })
+    }
+    
+    // scan user information
+    console.log(`scan user info`);
+    result = await readUserInfo();
+    result = JSON.parse(result);
+    
+    if(result['result'] == false){
+        displayReadValues('','','',code)
+    }else{
+        result = result['data']
+        displayReadValues(result[0] + ' ' + result[1],result[3],result[2],code)
+    }
+    
+    $('#position-modal').addClass('hidden');
+    $('#response-modal').removeClass('hidden');
+})
+
+readUserInfo = function(){
+    // request to read user information
+    return $.get("../core/operation.php", {
+        op: "info",
+        type: type
+    })
+}
+
+displayReadValues = function (name, nation, sex, code) {
+    $('#name-field input').val(name);
+    $('#nation-field input').val(nation);
+    $('#sex-field input').val(sex);
+    $('#cf-field input').val(code);
+}
+
+
+// SAVE DATA
+$('#response-modal button').click(() => {
+    // $('#response-modal').addClass('hidden');
+    localStorage.setItem('name', $('#name-input').val());
+    localStorage.setItem('nation', $('#nation-input').val())
+    localStorage.setItem('sex', $('#sex-input').val())
+    localStorage.setItem('code', $('#code-input').val())
+    saveData().then((message) => {
+        message = JSON.parse(message);
+        if (message['result'] == false) {
+            console.log(`ERRORE`);
+            //TODO show error message
+        } else {
+            window.location = './picture.html';
+        }
+    });
+})
+
+saveData = function () {
+    return $.post("../core/operation.php", {
+        op: "saveData",
+        name: localStorage.getItem('name'),
+        nation: localStorage.getItem('nation'),
+        sex: localStorage.getItem('sex'),
+        code: localStorage.getItem('code'),
+    }).done((message) => {
+        console.log(`message: ${message}`);
+        // window.location = 'index.html';
+    })
+}
---
+++ js/picture.js
@@ -0,0 +1,90 @@
+const webcamElement = document.getElementById('webcam');
+const canvasElement = document.getElementById('canvas');
+const snapSoundElement = document.getElementById('snapSound');
+const webcam = new Webcam(webcamElement, 'user', canvasElement);
+let picture;
+
+downloadImage = function (url, name) {
+    const link = document.createElement("a");
+    link.href = url;
+    link.download = name;
+    link.click();
+}
+
+startWebcam = function () {
+    console.log(`start`);
+    webcam.start()
+        .then(result => {
+            console.log("webcam started");
+        })
+        .catch(err => {
+            console.log(err);
+        });
+
+    $('#start-btn').hide();
+    $('#save-btn').hide();
+    $('#stop-btn').show();
+    $('#snap-btn').show();
+    $('#canvas').hide();
+    $('#webcam').show();
+}
+
+snapPhoto = function () {
+    picture = webcam.snap();
+    $('#start-btn').show();
+    $('#stop-btn').hide();
+    $('#snap-btn').hide();
+    $('#save-btn').show();
+    $('#webcam').hide();
+    $('#canvas').show();
+
+}
+
+savePhoto = function () {
+    // console.log(picture);
+    // downloadImage(picture, './picture/img1.png')
+
+    $.post("../core/operation.php", {
+        op: "saveImage",
+        image: picture,
+        name: localStorage.getItem('name')
+    }).done((message) => {
+        message = JSON.parse(message);
+        if (message['result'] == false) {
+            console.log('Error: '+ message['data']);
+            //TODO show error message
+        } else {
+            console.log(`Immagine salvata`);
+            window.location = 'index.html';
+        }
+    })
+}
+
+stopWebcam = function () {
+    webcam.stop();
+    $('#start-btn').show();
+    $('#stop-btn').hide();
+    $('#snap-btn').hide();
+    $('#save-btn').hide();
+    $('#webcam').hide();
+    $('#canvas').hide();
+}
+
+printInformation = function () {
+    $.post("../core/operation.php", {
+        op: "print",
+        name: localStorage.getItem('name'),
+        nation: localStorage.getItem('nation'),
+        sex: localStorage.getItem('sex'),
+        code: localStorage.getItem('code'),
+    }).done((message) => {
+        console.log(`message: ${message}`);
+        message = JSON.parse(message);
+        if (message['result'] == false) {
+            console.log(`${message['data']}`);
+            //TODO show error message
+        } else {
+            window.location = 'index.html';
+        }
+    })
+}
